f4e5e9bcdc9238b0879f3c98ffc04466992177e6
italiano@oplabs.co <italiano@oplabs.co>
2024-11-14 14:00:59 -0500

    Update tsup to support wasm

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,16 @@
 /* eslint-disable no-console */
 import { defineConfig } from 'tsup'
+import { wasmLoader } from 'esbuild-plugin-wasm'
+
+const wasmPlugin = wasmLoader({ mode: 'embedded' })
 
 export default defineConfig({
   name: 'super',
-  format: "esm",
-  entry: ['src/cli.tsx'],
+  format: 'esm',
+  entry: ['src/**/*.(ts|tsx)'],
   target: 'node18',
   splitting: false,
   sourcemap: true,
   clean: true,
+  esbuildPlugins: [wasmPlugin],
 })
